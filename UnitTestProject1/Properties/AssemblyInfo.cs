3ee838b2d62f2fd88144c35dcb2b2e41343b132d
ShunjiYamanaka <syun.singernet@gmail.com>
2020-03-26 09:16:31 +0900

    Chaining Assertionのテストロジックです。

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dbdec26a-1b97-48af-94a6-1c75e27226c1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(3, Class1.Add(1, 2));
+            //例外テスト（Chaining Assertion）
+            //AssertEx.Throws<InputException>(() => Class1.Add(-1, 2));
+
+            //戻り値もとることができる
+            var ex = AssertEx.Throws<InputException>(() => Class1.Add(-1, 2));
+            Assert.AreEqual("マイナス値は入力できません。", ex.Message);
+
+
+            //Chaining Assertionだけで場合
+            Class1.Add(1, 2).Is(3);
+            ex.Message.Is("マイナス値は入力できません。");
+        }
+
+        //例外テスト（InputExceptionが発生すればOK）
+        [TestMethod]
+        [ExpectedException(typeof(InputException))]
+        public void 例外テスト()
+        {
+            Assert.AreEqual(3, Class1.Add(-1, 2));
+        }
+    }
+}
